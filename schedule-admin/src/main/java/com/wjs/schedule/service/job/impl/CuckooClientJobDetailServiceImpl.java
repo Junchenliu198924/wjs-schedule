299181e2e095a51a76a780e799d323d65abdc34d
Silver <suzy@wjs.com>
2017-03-23 22:53:07 +0800

    package re-name

---
+++ schedule-admin/src/main/java/com/wjs/schedule/service/job/impl/CuckooClientJobDetailServiceImpl.java
@@ -0,0 +1,38 @@
+package com.wjs.schedule.service.job.impl;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wjs.schedule.dao.exec.CuckooClientJobDetailMapper;
+import com.wjs.schedule.domain.exec.CuckooClientJobDetail;
+import com.wjs.schedule.domain.exec.CuckooClientJobDetailCriteria;
+import com.wjs.schedule.service.job.CuckooClientJobDetailService;
+import com.wjs.schedule.vo.qry.JobClientQry;
+import com.wjs.util.dao.PageDataList;
+
+@Service("cuckooClientJobDetailService")
+public class CuckooClientJobDetailServiceImpl implements CuckooClientJobDetailService{
+	
+	@Autowired
+	CuckooClientJobDetailMapper cuckooClientJobDetailMapper;
+
+	@Override
+	public PageDataList<CuckooClientJobDetail> pageData(JobClientQry qry) {
+	
+		CuckooClientJobDetailCriteria crtPi = new CuckooClientJobDetailCriteria();
+		crtPi.setStart(qry.getStart());
+		crtPi.setLimit(qry.getLimit());
+		crtPi.setOrderByClause(" bean_name, method_name ");
+		CuckooClientJobDetailCriteria.Criteria crt = crtPi.createCriteria();
+		if(StringUtils.isNotEmpty(qry.getJobClassApplication())){
+			crt.andJobClassApplicationLike("%" + qry.getJobClassApplication() + "%");
+		}
+		if(StringUtils.isNotEmpty(qry.getJobName())){
+			crt.andJobNameLike("%" + qry.getJobName() + "%");
+		}
+		
+		return cuckooClientJobDetailMapper.pageByExample(crtPi);
+	}
+
+}
